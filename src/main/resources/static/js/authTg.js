ae0b0f2c33226571ad35b5926ff71d47f9f562c6
Radik <97843156+Rado700@users.noreply.github.com>
2025-04-05 19:24:31 +0300

    fix couch.js

--- src/main/resources/static/js/authTg.js
+++ src/main/resources/static/js/authTg.js
@@ -1,31 +1,35 @@
+
 let chatID;
+document.addEventListener('DOMContentLoaded', () => {
 
-try {
-    chatID = window.Telegram.WebApp.initDataUnsafe.user.id;
-} catch (error) {
-    chatID =  482133255;
-}
+    try {
+        chatID = window.Telegram.WebApp.initDataUnsafe.user.id;
+    } catch (error) {
+        console.error(error);
+    }
 
-function enterByTg (hashTgId){
-    fetch("/api/login/tg/enter/", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({tgId:hashTgId})
-    })
-        .then(response => {
-            return response.text();
-        }).then(data => {
-        window.location.href = data + window.location.search;
-    })
-}
+    function enterByTg(hashTgId) {
+        fetch("/api/login/tg/enter/", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({tgId: hashTgId})
+        })
+            .then(response => {
+                return response.text();
+            }).then(data => {
+            window.location.href = data + window.location.search;
+        })
+    }
 
-async function hashString(str) {
-    const encoder = new TextEncoder();
-    const data = encoder.encode(str);
-    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-}
-hashString(`${chatID}`).then(hash => enterByTg(hash));
+    async function hashString(str) {
+        const encoder = new TextEncoder();
+        const data = encoder.encode(str);
+        const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
+        return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    }
+
+    hashString(`${chatID}`).then(hash => enterByTg(hash));
+});
